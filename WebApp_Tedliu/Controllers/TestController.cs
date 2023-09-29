f2f8b97db200a715d0c5b7d8b4f9c22306ec56c2
劉敦桐 <zse811022@hotmail.com>
2023-09-29 21:02:24 +0800

    step1

---
+++ WebApp_Tedliu/Controllers/TestController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApp_Tedliu.Controllers
+{
+	public class TestController : Controller
+	{
+		public IActionResult Index()
+		{
+			// return View();
+			string todayeDates=DateTime.Now.ToShortDateString();
+			return Ok(todayeDates);
+		}
+	}
+}
---
+++ WebApplication1_.netcore/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WebApplication1_.netcore.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
