1e5c8ba1054f2a50d5eb06fb1f0ccec938e6111e
劉敦桐 <zse811022@hotmail.com>
2023-10-30 07:37:16 +0800

    Add_Dbcontext

---
+++ WebApp_RazePage/Data/ApplicationDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+using WebApp_RazePage.Model;
+
+namespace WebApp_RazePage.Data
+{
+    public class ApplicationDbContext:DbContext
+    {
+        public DbSet<Category> Categories { get; set; }
+    }
+}
