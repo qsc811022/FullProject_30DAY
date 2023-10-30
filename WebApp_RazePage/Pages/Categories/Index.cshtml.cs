589209514d684e957b4f7eeaae66583d238228a7
劉敦桐 <zse811022@hotmail.com>
2023-10-30 21:26:09 +0800

    Add Display all categories

--- WebApp_RazePage/Pages/Categories/Index.cshtml.cs
+++ WebApp_RazePage/Pages/Categories/Index.cshtml.cs
@@ -1,25 +1,20 @@
-﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 using WebApp_RazePage.Data;
 using WebApp_RazePage.Model;
 
-namespace WebApp_RazePage.Pages
+namespace WebApp_RazePage.Pages.Categories
 {
     public class IndexModel : PageModel
     {
         private readonly ApplicationDbContext _db;
-
-        public IEnumerable<Category> Categories { get; set; }
-        private readonly ILogger<IndexModel> _logger;
-
-
-
         public IndexModel(ApplicationDbContext db)
         {
-            _db= db;
+            _db = db;
         }
 
+        public IEnumerable<Category> Categories { get; set; }
         public void OnGet()
         {
             Categories = _db.Categories;
